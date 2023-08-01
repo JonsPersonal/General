96106a7cab73732e492671f92a367d6d335be619
Jon Edwards <jonathon.a.edwards@gmail.com>
2023-08-01 12:03:17 -0700

    Attempt 1

---
+++ discordBots/testBot/main.py
@@ -0,0 +1,15 @@
+import discord
+
+@client.event
+async def on_ready():
+    print("now runing")
+
+def main():
+    print("starting up the test bot")
+    client.run(MTEzNTk3MjkwMDA4NDY2NjQ3OQ.GluUcX.L6k6na5NnsS-lX45fOuRS4Hy-jJsFbI59MbCV8)
+    
+
+
+
+if __name__ == "__main__":
+    main()

492220b78d875459c49c61af2e7b5c3f0b8df5ac
JonsPersonal <jonathon.a.edwards@gmail.com>
2023-08-03 22:37:54 -0700

    sends a photo

--- discordBots/testBot/testbot.py
+++ discordBots/testBot/testbot.py
@@ -1,7 +1,21 @@
 import discord
 
-def run(TOKEN):
+async def sendPhoto(message):
+    try:
+            photo = discord.File('General/discordBots/testBot/photos/newfy.png')
+            
+            await message.channel.send(file = photo)
+            print(f"sent photos to {message.channel}")
+    except Exception as e :
+        print(e)
+    
+async def decodeCommand(message,command:str):
 
+    if(command == "sendPhotos"):
+        await sendPhoto(message)
+
+def run(TOKEN):
+    
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -10,4 +24,16 @@ def run(TOKEN):
     async def on_ready():
         print(f"i'm alive {client.user}")
 
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        print(f"{message.author} sent: '{message.content}' in {message.channel}")
+        inputstr = message.content
+        if(inputstr[0]=="!"):
+            await decodeCommand(message,inputstr[1:])
+        else:
+            return
+        
     client.run(TOKEN)

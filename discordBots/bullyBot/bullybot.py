300662a8ff9ad05bc3f9ed6c336fd82e77fd9e4e
Jon Edwards <jonathon.a.edwards@gmail.com>
2024-01-20 13:23:36 -0800

    structor of drawing and advising

--- discordBots/bullyBot/bullybot.py
+++ discordBots/bullyBot/bullybot.py
@@ -1,16 +1,41 @@
 import tokens as Token
 import discord
 import chatController as chat
+import random
 
 async def bullyRelpy(message):
-    response = chat.bullyThemText(f"{message.author}",f"{message.content}", Token.getChatgptToken())
+
+    tester = random.randint(0,10)
+    print(f"responds if 1 rolled a: {tester}")
+    if(tester == 1):
+        response = chat.bullyThemText(f"{message.author}",f"{message.content}", Token.getChatgptToken())
+        try:
+            await message.reply(response)
+            print(f"message responsed in {message.channel}")
+        except Exception as e :
+           print(e)
+
+async def adviceRelpy(message):
+    input = message.content.split()[1:]
+    response = chat.adviseThem(f"{message.author}",input, Token.getChatgptToken())
     try:
         await message.reply(response)
         print(f"message responsed in {message.channel}")
     except Exception as e :
-        print(e)
+           print(e)
+
+async def drawRelpy(message):
+    response = chat.drawThem(f"{message.author}",f"{message.content}", Token.getChatgptToken())
 
 
+async def decode(message,inputstr):
+    if(inputstr == "advice"):
+        ##await adviceRelpy(message)
+        print("worked")
+    if(inputstr == "draw"):
+        ##await drawRelpy(message)
+        print("draw worked")
+        
 def run():
     print("booting up the bully bot!")
     intents = discord.Intents.default()
@@ -27,9 +52,12 @@ def run():
             return
         
         print(f"{message.author} sent: '{message.content}' in {message.channel}")
-
-        await bullyRelpy(message)
-        
+        if(f"{message.channel}" == 'cancerous-content'):
+            await bullyRelpy(message)
+        inputstr = message.content
+        if(inputstr[0]=="!"):
+            command = inputstr[1:].split()[0]
+            await decode(message,command)
     client.run(Token.getBotToken())
 
 def main():
--- discordBots/bullyBot/chatController.py
+++ discordBots/bullyBot/chatController.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 
 def bullyThemText(user, message,token):
     
-    content = "in less than 2000 characters roast the following user in the style of an extremely aggressive gamer who reallly hates everyone that isn't them:\" "+user+" \" for sending the message:\""+message
+    content = "in less than 2000 characters roast the following user in the style of an extremely aggressive gamer who really hates everyone that isn't them:\" "+user+" \" for sending the message:\""+message
     client = OpenAI(
     # This is the default and can be omitted
     api_key=token
